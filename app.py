b9fd3969b3632f331537bf4bbd77e330ed57c711
Jialong Wu <callanwu@users.noreply.huggingface.co>
2025-09-18 21:39:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def app_gui():
         message=messages,
         share=False,
         server_name='127.0.0.1',
-        server_port=7860,
+        server_port=8080,
         concurrency_limit=20,
         enable_mention=False,
     )
